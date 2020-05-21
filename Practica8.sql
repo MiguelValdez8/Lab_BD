38645ba398f052c5b19908667d6be793d494ac0a
Miguel Valdez <00mike98@gmail.com>
2020-05-21 13:27:17 -0500

    Comandos de la practica 8

---
+++ Practica8.sql
@@ -0,0 +1,52 @@
+USE Libreria
+
+SELECT *
+FROM students s
+LEFT JOIN borrows bw
+ON s.studentId = bw.studentId;
+
+SELECT *
+FROM authors a
+LEFT JOIN type t
+ON a.authorId = t.typeId;
+
+SELECT *
+FROM students s
+RIGHT JOIN borrows bw
+ON s.studentId = bw.studentId;
+
+SELECT *
+FROM authors a
+RIGHT JOIN type t
+ON a.authorId = t.typeId;
+
+SELECT *
+FROM students s
+INNER JOIN borrows bw
+ON s.studentId = bw.studentId;
+
+SELECT *
+FROM authors a
+INNER JOIN books b
+ON a.authorId = b.bookId;
+
+SELECT * 
+FROM books 
+CROSS JOIN type;
+
+SELECT * 
+FROM students 
+CROSS JOIN borrows;
+
+SELECT name, typeId FROM books WHERE pagecount > (SELECT AVG(pagecount) FROM books);
+
+CREATE TABLE authorsBackUp(
+	[authorId] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](50) NULL,
+	[surname] [varchar](70) NULL,)
+
+DROP TABLE authorsBackUp;
+
+SELECT * INTO authorsBackUp FROM authors
+
+SELECT * FROM authorsBackUp
